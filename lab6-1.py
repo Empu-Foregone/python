678151e059fb9870b8547858b324f6876a84bf6f
Empu-Foregone <149968740+Empu-Foregone@users.noreply.github.com>
2024-11-29 02:21:47 +0200

    lab6-1.py

--- lab6-1.py
+++ lab6-1.py
@@ -18,13 +18,10 @@ def insert_element(lst, index, element):
 
   return result
 
-# Отримання списку від користувача
 my_list = list(map(int, input("Введіть елементи списку через пробіл: ").split()))
 
-# Отримання індексу та елемента для вставки
 index = int(input("Введіть індекс для вставки: "))
 element = int(input("Введіть елемент для вставки: "))
 
-# Виклик функції та друк результату
 result_list = insert_element(my_list, index, element)
 print("Результат:", result_list)
